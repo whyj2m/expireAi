35fbae5df13d9429c935565922b621c613514608
whyj2m <149341808+whyj2m@users.noreply.github.com>
2025-08-27 14:21:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from datetime import datetime
 
-if name == "main": print("Expiry AI Reporter started at:", datetime.now().isoformat())
+if __name__ == "__main__":
+    print("Expiry AI Reporter started at:", datetime.now().isoformat())
